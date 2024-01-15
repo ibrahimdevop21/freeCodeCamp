f23857069642ef5aa59a8ba00449aa5b45fd96f7
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-01-15 22:26:12 +0200

    "first commit for the tribute page"

---
+++ tribute/index.js
@@ -0,0 +1,29 @@
+
+<main id="main">
+<h1>Mohammed Ahmad Mahgoub</h1>
+<h2 id="tribute-info">The last honorable Prime Minister of Sudan</h2>
+<div id="img-div">
+    <img src="https://scontent.famm6-1.fna.fbcdn.net/v/t39.30808-6/302327244_616079550081284_4584324192399445540_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGOExHAurA3YrL6ZyJfqxSBvU3668u1alq9Tfrry7VqWgZxXEmDrrqoAXxZrw-xlc4&_nc_ohc=rVricBQd8H4AX9gcqog&_nc_ht=scontent.famm6-1.fna&oh=00_AfArIqjnIp0tf0CYFaGZBtbIHqPWv8e__JuNgjnujtsqgA&oe=64C4EA2C" alt="Leaders from left to right: King Faisal of Saudi Arabia, Col. Jamal Abdul Nasser of Egypt, King Hussein of Jordan, Prime Minister Muhammad Mahgoub of Sudan" id="image">
+    <figcaption id="img-caption">
+        From left to right:<br>
+        King Faisal of Saudi Arabia<br>
+        Col. Jamal Abdul Nasser of Egypt<br>
+        King Hussein of Jordan<br>
+        Prime Minister Muhammad Mahgoub of Sudan
+    </figcaption>
+</div>
+<div class="tribute-info">
+    <p>
+        Muhammad Ahmad Mahgoub (Arabic: محمد أحمد المحجوب; 17 May 1908[1] – 23 June 1976[2]) was both Foreign Minister and then the 6th Prime Minister of Sudan. He was also an important Sudanese literary writer, who published several volumes of poetry and literary criticism in Arabic.
+    </p>
+    <p>
+        He was born in the city of Aldewen in 1908. He moved to Khartoum at the age of seven. Mahgoub graduated from engineering school in 1929 and in 1938, he obtained a Bachelor of Laws degree from the Gordon Memorial College. He was elected to parliament in 1946. After independence, Mahgoub was foreign minister between 1956 and 1958, and then again between 1964 and 1965.
+    </p>
+    <p>
+        In 1965, he was elected Prime Minister, but subsequently forced to resign. In 1967, he was elected Prime Minister for the second time and served in that position until 1969.
+    </p>
+</div>
+<p>
+    If you have time, you should read more about this incredible human being on his <a id="tribute-link" href="https://en.wikipedia.org/wiki/Muhammad_Ahmad_Mahgoub#Further_reading" target="_blank">Wikipedia entry</a>.
+</p>
+</main>
