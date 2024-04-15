7dc86c2cc1e6f10685a6d1ab172da6ce6ee45fb8
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-04-16 01:56:34 +0200

    4th day of the challenge

---
+++ platformerGame/script.js
@@ -0,0 +1,23 @@
+const startBtn = document.getElementById('start-btn');
+const canvas = document.getElementById('canvas');
+const startScreen = document.querySelector('.start-screen');
+const checkpointScreen = document.querySelector('.checkpoint-screen');
+const checkpointMessage = document.querySelector('.checkpoint-screen > p');
+const ctx = canvas.getContext('2d');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5;
+let isCheckpointCollisionDetectionActive = true;
+
+const proportionalSize = (size) => {
+  return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
+};
+
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const player = new Player();
