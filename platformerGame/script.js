d88d9f3771706f4b952071e4f92f6f19e780940f
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-04-18 02:20:09 +0200

    6th day of the challange

--- platformerGame/script.js
+++ platformerGame/script.js
@@ -14,10 +14,69 @@ const proportionalSize = (size) => {
 };
 
 class Player {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+  constructor() {
+    this.position = {
+      x: proportionalSize(10),
+      y: proportionalSize(400),
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(40);
+  }
+  draw() {
+    ctx.fillStyle = '#99c9ff';
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      if (this.position.y < 0) {
+        this.position.y = 0;
+        this.velocity.y = gravity;
+      }
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+
+    if (this.position.x < this.width) {
+      this.position.x = this.width;
+    }
+
+    if (this.position.x >= canvas.width - 2 * this.width) {
+      this.position.x = canvas.width - 2 * this.width;
+    }
   }
 }
 
 const player = new Player();
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+};
+
+const keys = {
+  rightKey: {
+    pressed: false,
+  },
+  leftKey: {
+    pressed: false,
+  },
+};
+
+const startGame = () => {
+  canvas.style.display = 'block';
+  startScreen.style.display = 'none';
+  player.draw();
+};
+
+startBtn.addEventListener('click', startGame);
