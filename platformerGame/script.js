cbe74a29076197085517e2e550f69d4c08d03a89
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-04-19 02:14:03 +0200

    day 7

--- platformerGame/script.js
+++ platformerGame/script.js
@@ -62,6 +62,17 @@ const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+
+  if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+    player.velocity.x = 5;
+  } else if (
+    keys.leftKey.pressed &&
+    player.position.x > proportionalSize(100)
+  ) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
 };
 
 const keys = {
@@ -71,6 +82,29 @@ const keys = {
   leftKey: {
     pressed: false,
   },
+};
+
+const movePlayer = (key, xVelocity, isPressed) => {
+  if (!isCheckpointCollisionDetectionActive) {
+    player.velocity.x = 0;
+    player.velocity.y = 0;
+    return;
+  }
+
+  switch (key) {
+    case 'ArrowLeft':
+      keys.leftKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x -= xVelocity;
+      break;
+    case 'ArrowUp':
+    case ' ':
+    case 'Spacebar':
+      player.velocity.y -= 8;
+      break;
+  }
 };
 
 const startGame = () => {
