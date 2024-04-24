3eaaf90ab1d2b7f14bbc56f764768fb1033cf960
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-04-25 01:45:39 +0200

    diceGame done

--- diceGame/script.js
+++ diceGame/script.js
@@ -90,6 +90,36 @@ const detectFullHouse = (arr) => {
   for (const num of arr) {
     counts[num] = counts[num] ? counts[num] + 1 : 1;
   }
+
+  const hasThreeOfAKind = Object.values(counts).includes(3);
+  const hasPair = Object.values(counts).includes(2);
+
+  if (hasThreeOfAKind && hasPair) {
+    updateRadioOption(2, 25);
+  }
+
+  updateRadioOption(5, 0);
+};
+
+const checkForStraights = (arr) => {
+  const sortedNumbersArr = arr.sort((a, b) => a - b);
+  const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+  const uniqueNumbersStr = uniqueNumbersArr.join('');
+
+  const smallStraightsArr = ['1234', '2345', '3456'];
+  const largeStraightsArr = ['12345', '23456'];
+
+  if (
+    smallStraightsArr.some((straight) => uniqueNumbersStr.includes(straight))
+  ) {
+    updateRadioOption(3, 30);
+  }
+
+  if (largeStraightsArr.includes(uniqueNumbersStr)) {
+    updateRadioOption(4, 40);
+  }
+
+  updateRadioOption(5, 0);
 };
 
 const resetRadioOption = () => {
@@ -132,6 +162,7 @@ rollDiceBtn.addEventListener('click', () => {
     rollDice();
     updateStats();
     getHighestDuplicates(diceValuesArr);
+    detectFullHouse(diceValuesArr);
   }
 });
 
